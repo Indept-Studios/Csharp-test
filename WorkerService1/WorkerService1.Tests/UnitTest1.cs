4d92c4c72caf9adb518e2fb05529041e0b213495
Chris Wesch <34998667+Indept-Studios@users.noreply.github.com>
2022-01-04 19:50:13 +0100

    feat: added solution with project

---
+++ WorkerService1/WorkerService1.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace WorkerService1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
